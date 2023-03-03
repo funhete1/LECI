4ee787217cdb7c0264d26c8d05f1cff74f1553ff
funhete1 <51arthurm@gmail.com>
2023-03-03 14:13:10 +0000

    .

--- Ano2/S2/ACII/Aula03/Part2/Ex1_1.s
+++ Ano2/S2/ACII/Aula03/Part2/Ex1_1.s
@@ -46,16 +46,3 @@ while:
 
 ###delay method 
 #$a0 - mss
-delay:                                  # void delay(int ms) {
-        move    $t0, $a0                #       $t0 = ms;
-        
-for:    ble     $t0, 0, endfor          #       for(; ms > 0; ms--) {
-        li      $v0, 12   #               
-        syscall                         #               resetCoreTimer();
-read:   li      $v0, 11    #
-        syscall                         #               readCoreTimer();
-        blt     $v0, 20000, read        #               while(readCoreTimer() < K);
-        addi    $t0, $t0, -1            #               ms--;
-        j       for                     #       }
-endfor:                                 #       
-        jr      $ra 
